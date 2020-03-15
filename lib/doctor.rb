75e485ddd369ce2b13bbdd0a770a5fd26b192da9
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-15 18:05:54 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,37 @@
+class Doctor
+ #attr_accessor :name, :patient, :doctor, :date
+ attr_reader :name, :appointment, :patient
+
+  @@all_doc = []
+
+  def initialize(name)
+    @name = name
+    @@all_doc << self
+  end
+
+  def self.all
+    @@all_doc
+  end
+
+  def new_appointment(patient, date)
+    appointment = Appointment.new(patient, self, date)
+    self.appointments.last
+  end
+  
+  
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
+
+end
